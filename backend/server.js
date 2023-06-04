a1d41c5e0b5da98097ce87a6be2fd9087b4e3742
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-06-04 21:12:36 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 5001;
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join('frontend/build')));
+  app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'frontend/build','index.html')));
+}
+
+
+
 app.get('/',async(req,res)=>{
     res.sendFile('./frontend/src/pages/LoginPage.js')
 }
