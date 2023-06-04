4947f3f9e2fa324929bab95c67a933976115fe41
AKN414-IND <85106890+AKN414-IND@users.noreply.github.com>
2023-06-04 22:54:03 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const app = express();
+const path = require('path');
 
+const app = express();
+const port = process.env.PORT || 5001;
+
+// Middleware
 app.use(express.json());
 app.use(cors());
 
+// Connect to MongoDB
 const dbURI = 'mongodb+srv://arunknair2003:WQxDAYa5EBEWBJGZ@cluster0.dbylnkz.mongodb.net/Arun';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(5001, () => {
-      console.log("Server is listening on port 5001");
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
     });
   })
   .catch((err) => {
     console.log("Error connecting to MongoDB: ", err);
   });
 
+// API routes
 const usersRoutes = require('./routes/users');
 const blogsRoutes = require('./routes/blogs');
 app.use('/api/users', usersRoutes);
 app.use('/api/blogs', blogsRoutes);
 
-module.exports = app;
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+}
